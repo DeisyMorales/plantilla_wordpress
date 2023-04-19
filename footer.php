00989d5df685a719d63813079e148b96d1b215f4
DeisyMorales <deysimorales0102@gmail.com>
2023-04-19 09:44:19 -0500

    subir primer plantilla

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-white text-center py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p> Pie de pagina</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
---
+++ index.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mi Pagina Web</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  
+<body>
+
+<?php include('heeder.php'); ?>
+
+
+<div class="container my=3">
+    
+     <h1>ROPAS PARA MUJERES</h1> <br>
+    
+     <div class="row">
+
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="imagenes/img.jpg" alt="Imagen 1" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">SODA BLUES</h5>
+             <p class="card-text">CASACA SODA BLUES PARA MUJER HURLEY
+        SKU:2015310132646
+        
+        No reseñas
+        -
+        Internet
+        S/ 169.99</p>
+             <a href="indexx.php" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir</a>
+           </div>
+         </div>
+       </div>
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="imagenes/img1.jpg" alt="Imagen 2" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">BARBADOS</h5>
+             <p class="card-text">POLERA BARBADOS PARA MUJER ESTEVA
+        SKU:2015299602291
+        
+        No reseñas
+        -
+        Normal
+        S/ 189.99
+        Internet
+        S/ 149.99</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir</a>
+           </div>
+         </div>
+       </div>
+      
+      
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="imagenes/img2.jpg" alt="Imagen 3" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">MARQUIS</h5>
+             <p class="card-text">CHOMPAS MARQUIS PARA MUJER EHALF
+        SKU:2074303277429
+        
+        No reseñas
+        -
+        Normal
+        S/ 149.99
+        Internet
+        S/ 119.99</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir</a>
+           </div>
+         </div>
+        </div>
+        
+       
+     
+     
+    
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="imagenes/img3.jpg" alt="Imagen 4" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">INDEX</h5>
+             <p class="card-text">PANTALÓN DE BUZO INDEX PARA MUJER PUÑO ELÁSTICO
+        SKU:2015300714050
+        
+        No reseñas
+        Características destacadas
+        Marca: INDEX
+        Material: 60% Algodón Bci / 40% Poliéster
+        Ver más características
+        Normal
+        S/ 99.99
+        Internet
+        S/ 59.99</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir</a>
+           </div>
+         </div>
+       </div>
+      
+       
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="imagenes/img4.jpg" alt="Imagen 5" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">CACHAREL</h5>
+             <p class="card-text">JEAN CACHAREL PARA MUJER UPPER
+        SKU:2070283071835
+        
+        4 reseñas
+        Características destacadas
+        Material: 98% Algodón - 2% Elastano
+        Ver más características
+        Normal
+        S/ 119.99
+        Internet
+        S/ 69.99</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir</a>
+           </div>
+         </div>
+       </div>
+       
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="imagenes/img5.jpg" alt="Imagen 6" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">INDEX</h5>
+             <p class="card-text">CHOMPA INDEX PARA MUJER CUELLO REDONDO DISEÑO FESALTO
+        SKU:2015305353865
+        
+        No reseñas
+        -
+        Internet
+        S/ 99.99</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir</a>
+           </div>
+         </div>
+       </div>
+       </div> 
+       </div><br>
+      
+
+       <?php include('footer.php'); ?>
+ 
+       
+  
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+
+
+
+</body>
+
